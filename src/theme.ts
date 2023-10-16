6166973f89b96298d62708eae410caff0bc00a51
Andrew Boyd <andrew@formkit.com>
2023-10-16 15:20:44 -0400

    feat: adds datepicker styles

--- src/theme.ts
+++ src/theme.ts
@@ -57,7 +57,7 @@ export default createTheme({
     textSize: {
       editor: "fontSize",
       value: "base",
-      min: "xs",
+      min: "sm",
       max: "base",
     },
     inputShadow: {
@@ -87,17 +87,17 @@ export default createTheme({
       outer:
         "group mb-$spacing(5,*) data-[disabled]:select-none data-[disabled]:opacity-50 text-$textSize",
       label:
-        "block text-$textColor-700 text-$textSize(-1) font-bold mb-$spacing(-2) dark:text-$textColor-300",
+        "block text-$textColor-700 text-$textSize(-1,*) font-bold mb-$spacing(-2) dark:text-$textColor-300",
       legend:
-        "block text-$textColor-700 text-$textSize(-1) font-bold dark:text-$textColor-300",
+        "block text-$textColor-700 text-$textSize(-1,*) font-bold dark:text-$textColor-300",
       input: "appearance-none [color-scheme:light] dark:[color-scheme:dark]",
       prefixIcon:
         "flex items-center mr-$spacing(0) text-$textSize h-[1em] w-[1em] shrink-0 [&>svg]:w-full",
       suffixIcon:
         "flex items-center ml-$spacing(0) text-$textSize h-[1em] w-[1em] shrink-0 [&>svg]:w-full",
-      help: "text-$textColor-500 text-$textSize(-2) dark:text-$textColor-400",
+      help: "text-$textColor-500 text-$textSize(-2,*) dark:text-$textColor-400",
       message:
-        "text-red-500 mb-$spacing(-1) text-$textSize(-2) dark:text-red-400",
+        "text-red-500 mb-$spacing(-1) text-$textSize(-2,*) dark:text-red-400",
     },
 
     /**
@@ -110,7 +110,7 @@ export default createTheme({
       outer: "data-[disabled]:opacity-100",
       input: `
         ${"" /* base styles */}
-        bg-$primaryColor-500 $borderRadius !text-$textSize(-1) p-$spacing(3,1,5) outline-none flex items-center mb-$spacing(-1) text-white text-$textSize(-1) active:text-$primaryColor-100 active:bg-$primaryColor-700 hover:bg-$primaryColor-600 ring-offset-2 ring-$primaryColor-400 focus-visible:ring-2 disabled:border-$textColor-$borderColorStrength disabled:bg-$textColor-400 disabled:text-$textColor-100 group-data-[disabled]:!cursor-not-allowed $inputShadow
+        bg-$primaryColor-500 $borderRadius !text-$textSize(-1,*) p-$spacing(3,1,5) outline-none flex items-center mb-$spacing(-1) text-white text-$textSize(-1,*) active:text-$primaryColor-100 active:bg-$primaryColor-700 hover:bg-$primaryColor-600 ring-offset-2 ring-$primaryColor-400 focus-visible:ring-2 disabled:border-$textColor-$borderColorStrength disabled:bg-$textColor-400 disabled:text-$textColor-100 group-data-[disabled]:!cursor-not-allowed $inputShadow
       
         ${"" /* dark mode styles */}
         dark:disabled:border-$textColor-100 dark:disabled:bg-$textColor-500 dark:disabled:text-$textColor-200 dark:ring-offset-$primaryColor-300
@@ -138,12 +138,12 @@ export default createTheme({
         "group/listoption mb-$spacing last:mb-0 data-[disabled]:opacity-50 data-[disabled]:select-none group-data-[disabled]:data-[disabled]:opacity-100",
       label: `
         ${"" /* base styles */}
-        !mb-0 !font-normal !text-$textSize(-1)
+        !mb-0 !font-normal !text-$textSize(-1,*)
 
         ${"" /* dark mode styles */}
         dark:text-$textColor-300
       `,
-      optionHelp: "text-$textColor-500 text-$textSize(-2) mt-$spacing(-3)",
+      optionHelp: "text-$textColor-500 text-$textSize(-2,*) mt-$spacing(-3)",
       help: "mb-$spacing(-3) group-data-[multiple]:mb-$spacing group-data-[multiple]:-mt-$spacing",
     },
     "family:text": {
@@ -215,9 +215,9 @@ export default createTheme({
       fileItem:
         "flex min-w-0 items-center text-$textColor-700 mb-$spacing(-1) last:mb-0 dark:text-$textColor-300",
       fileName:
-        "truncate min-w-0 w-full shrink leading-5 group-data-[has-multiple]/list:text-$textSize(-1)",
+        "truncate min-w-0 w-full shrink leading-5 group-data-[has-multiple]/list:text-$textSize(-1,*)",
       fileRemove:
-        "right-$spacing peer-data-[has-multiple]:text-$textSize(-1) peer-data-[has-multiple]:bottom-$spacing peer-data-[has-multiple]:left-$spacing group-data-[prefix-icon]:peer-data-[has-multiple]:left-$spacing(10,*) peer-data-[has-multiple]:text-$primaryColor-500 ring-$primaryColor-400 $borderRadius z-20 flex appearance-none items-center text-[0px] outline-none hover:!text-red-500 focus-visible:ring-2 group-data-[disabled]:pointer-events-none peer-data-[has-multiple]:absolute group-data-[disabled]:!text-$textColor-500 dark:hover:!text-red-400",
+        "right-$spacing peer-data-[has-multiple]:text-$textSize(-1,*) peer-data-[has-multiple]:bottom-$spacing peer-data-[has-multiple]:left-$spacing group-data-[prefix-icon]:peer-data-[has-multiple]:left-$spacing(10,*) peer-data-[has-multiple]:text-$primaryColor-500 ring-$primaryColor-400 $borderRadius z-20 flex appearance-none items-center text-[0px] outline-none hover:!text-red-500 focus-visible:ring-2 group-data-[disabled]:pointer-events-none peer-data-[has-multiple]:absolute group-data-[disabled]:!text-$textColor-500 dark:hover:!text-red-400",
       fileRemoveIcon: "block text-base w-[0.75em] relative z-10",
       inner:
         "relative cursor-pointer group-data-[multiple]:$borderRadius(0,rounded-none,rounded-2xl)",
@@ -282,10 +282,10 @@ export default createTheme({
       selectIcon:
         "absolute w-[1em] text-$textColor-700 pointer-events-none right-$spacing group-data-[suffix-icon]:mr-[1.5em] dark:text-$textColor-300",
       optGroup:
-        "group/optgroup group-data-[multiple]:px-$spacing(-2) pt-$spacing(-2) font-bold text-$textSize(-1)",
+        "group/optgroup group-data-[multiple]:px-$spacing(-2) pt-$spacing(-2) font-bold text-$textSize(-1,*)",
       option: `
         ${"" /* base styles */}
-        group-data-[disabled]:opacity-50 group-data-[disabled]:select-none group-data-[multiple]:checked:bg-$primaryColor-100 group-data-[multiple]:focus:bg-$primaryColor-100 group-data-[multiple]:text-$textSize(-1) group-data-[multiple]:outline-none group-data-[multiple]:border-none group-data-[multiple]:py-$spacing(-1) group-data-[multiple]:px-$spacing
+        group-data-[disabled]:opacity-50 group-data-[disabled]:select-none group-data-[multiple]:checked:bg-$primaryColor-100 group-data-[multiple]:focus:bg-$primaryColor-100 group-data-[multiple]:text-$textSize(-1,*) group-data-[multiple]:outline-none group-data-[multiple]:border-none group-data-[multiple]:py-$spacing(-1) group-data-[multiple]:px-$spacing
 
         ${"" /* dark mode styles */}
         dark:group-data-[multiple]:focus:bg-$primaryColor-700 dark:group-data-[multiple]:checked:bg-$primaryColor-700
@@ -301,7 +301,7 @@ export default createTheme({
         "flex $inputMaxWidth items-center mb-$spacing(-1) bg-white border border-$textColor-$borderColorStrength $borderRadius(0,rounded-none,rounded-2xl) focus-within:ring-1 focus-within:!ring-$primaryColor-400 focus-within:!border-$primaryColor-400 group-data-[invalid]:border-red-500 group-data-[invalid]:ring-1 group-data-[invalid]:ring-red-500 group-data-[disabled]:bg-$textColor-100 $inputShadow dark:border-$textColor-$borderColorStrength(2) dark:group-data-[disabled]:bg-$textColor-800/5 dark:group-data-[invalid]:border-red-400 dark:group-data-[invalid]:ring-red-400 dark:bg-transparent",
       input: `
         ${"" /* base styles */}
-        text-$textSize(-1) h-$spacing(16,*) text-$textColor-700 min-w-0 grow shrink !p-$spacing outline-none bg-transparent selection:bg-$primaryColor-100 placeholder:text-$textColor-400 group-data-[disabled]:!cursor-not-allowed
+        text-$textSize(-1,*) h-$spacing(16,*) text-$textColor-700 min-w-0 grow shrink !p-$spacing outline-none bg-transparent selection:bg-$primaryColor-100 placeholder:text-$textColor-400 group-data-[disabled]:!cursor-not-allowed
 
         ${"" /* dark mode styles */}
         dark:placeholder-$textColor-400/50 dark:text-$textColor-300
@@ -377,7 +377,7 @@ export default createTheme({
       tag: "flex items-center cursor-default rounded-full text-$textSize(-1,xs,sm) px-$spacing(-1,1,2) py-$spacing(-2,0.5,1.5) mr-$spacing(-2,0.5,1.5) mb-$spacing(-2,0.5,1.5) bg-$primaryColor-500 text-white [&>.formkit-removeSelection]:!w-[0.5em] [&>.formkit-removeSelection]:aspect-square [&>.formkit-removeSelection]:!text-inherit [&>.formkit-removeSelection]:cursor-pointer",
       tagLabel: "mr-1",
       emptyMessage:
-        "flex items-center px-$spacing py-$spacing(-1,1) first:pt-$spacing last:pb-$spacing text-$textColor-700 text-$textSize(-1) aria-selected:text-white aria-selected:bg-$primaryColor-500",
+        "flex items-center px-$spacing py-$spacing(-1,1) first:pt-$spacing last:pb-$spacing text-$textColor-700 text-$textSize(-1,*) aria-selected:text-white aria-selected:bg-$primaryColor-500",
     },
     autocomplete: {
       // inherits family:dropdown classes
@@ -386,6 +386,57 @@ export default createTheme({
       selectionWrapper:
         "bg-$textColor-100 $borderRadius group-data-[multiple]:border group-data-[multiple]:border-$textColor-$borderColorStrength(-1,200,600) group-data-[multiple]:mb-$spacing(-1,0.5,2)",
       selection: "$borderRadius just pl-$spacing [&>*]:ml-0",
+    },
+    datepicker: {
+      inner: "relative pl-0",
+      panelWrapper:
+        "absolute min-w-[22em] top-[calc(100%_+_0.5em)] shadow-[0_0_1.25em_rgba(0,0,0,.25)] $borderRadius(0,rounded-none,rounded-2xl) p-$spacing(4,*) bg-white z-10",
+      panelHeader:
+        "grid grid-cols-[2.5em_1fr_2.5em] justify-center items-center border-b-2 border-$textColor-$borderColorStrength(-1,200,600) mb-$spacing pb-$spacing(1,*)",
+      panel: "flex justify-center",
+      input: "pl-$spacing placeholder:text-$textColor-400",
+      monthsHeader: "flex items-center justify-center col-start-2 col-end-2",
+      timeHeader: "flex items-center justify-center col-start-2 col-end-2",
+      overlayPlaceholder: "text-$textColor-400",
+      overlayLiteral: "text-$textColor-700",
+      overlayEnum: "text-$textColor-700",
+      months: "grid grid-cols-3 w-full",
+      month:
+        "m-$spacing(-1) p-$spacing(-2) text-center text-$textColor-700 $borderRadius bg-$textColor-200 aria-selected:bg-$primaryColor-500 aria-selected:text-white focus:outline focus:outline-2 focus:outline-$primaryColor-500 focus:outline-offset-2 focus:bg-white focus:text-$textColor-700 data-[is-extra=true]:opacity-25 group-data-[disabled]:opacity-50 group-data-[disabled]:cursor-default group-data-[disabled]:pointer-events-none",
+      yearsHeader:
+        "flex items-center justify-center text-$textColor-700 col-start-2 col-end-2",
+      years: "grid grid-cols-5 w-full",
+      year: "text-$textSize text-center text-$textColor-700 items-center m-$spacing(-1) p-$spacing(-2) $borderRadius bg-$textColor-200 aria-selected:bg-$primaryColor-500 aria-selected:text-white focus:outline focus:outline-2 focus:outline-$primaryColor-500 focus:outline-offset-2 focus:bg-white data-[is-extra=true]:opacity-25 group-data-[disabled]:opacity-50 group-data-[disabled]:cursor-default group-data-[disabled]:pointer-events-none",
+      weekDays: "grid grid-cols-7",
+      weekDay:
+        "w-[2.25em] text-$textColor-700 m-$spacing(-1) $borderRadius font-medium lowercase",
+      calendarWeeks: "",
+      week: "grid grid-cols-7 group-data-[disabled]:opacity-50 group-data-[disabled]:cursor-default group-data-[disabled]:pointer-events-none",
+      dayCell:
+        "flex items-center justify-center text-center text-$textColor-700 w-[2.25em] h-[2.25em] m-1 p-$spacing $borderRadius bg-$textColor-200 aria-selected:bg-$primaryColor-500 aria-selected:text-white focus:outline focus:outline-2 focus:outline-$primaryColor-500 focus:outline-offset-2 focus:bg-white data-[is-extra=true]:opacity-25 formkit-disabled:opacity-50 formkit-disabled:cursor-default formkit-disabled:pointer-events-none",
+      timeInput:
+        "w-full border-2 border-$textColor-$borderColorStrength(-1,200,600) $borderRadius p-$spacing my-$spacing(5,*) focus-visible:outline-$primaryColor-500",
+      daysHeader: "flex items-center justify-center",
+      prev: "mr-auto px-$spacing(1,*) py-$spacing(-3,*) hover:bg-$textColor-100 $borderRadius col-start-1 col-end-1",
+      prevLabel: "hidden",
+      prevIcon:
+        "flex w-[0.75em] select-none text-$textColor-700 [&>svg]:w-full",
+      dayButton:
+        "appearance-none text-$textColor-700 cursor-pointer px-$spacing(1,*) py-$spacing(-3,*) border-2 border-$textColor-$borderColorStrength(-1,200,600) $borderRadius mx-1 hover:border-$primaryColor-500",
+      monthButton:
+        "appearance-none text-$textColor-700 cursor-pointer px-$spacing(1,*) py-$spacing(-3,*) border-2 border-$textColor-$borderColorStrength(-1,200,600) $borderRadius mx-1 hover:border-$primaryColor-500",
+      yearButton:
+        "appearance-none text-$textColor-700 cursor-pointer px-$spacing(1,*) py-$spacing(-3,*) border-2 border-$textColor-$borderColorStrength(-1,200,600) $borderRadius mx-1 hover:border-$primaryColor-500",
+      next: "ml-auto px-$spacing(1,*) py-$spacing(-3,*) hover:bg-$textColor-100 hover:rounded col-start-3 col-end-3",
+      nextLabel: "hidden",
+      nextIcon:
+        "flex w-[0.75em] select-none text-$textColor-700 [&>svg]:w-full",
+      openButton: `
+        appearance-none border-0 bg-transparent flex p-0 self-stretch cursor-pointer
+        focus-visible:outline-none focus-visible:text-white focus-visible:bg-$primaryColor-500
+      `,
+      calendarIcon:
+        "text-$textColor-600 flex w-8 grow-0 shrink-0 self-stretch select-none [&>svg]:w-full [&>svg]:m-auto [&>svg]:max-h-[1em] [&>svg]:max-w-[1em]",
     },
     dropdown: {
       // inherits family:dropdown classes
@@ -403,7 +454,7 @@ export default createTheme({
       selections: "inline-flex items-center",
       selectionsItem: "whitespace-nowrap mr-1",
       truncationCount:
-        "flex items-center justify-center h-[1.5em] $borderRadius bg-$textColor-400 text-white whitespace-nowrap text-$textSize(-3) tracking-tighter leading-0 py-$spacing(-4,0.5,1) px-$spacing(-2,0.5,1) shrink-0 my-auto",
+        "flex items-center justify-center h-[1.5em] $borderRadius bg-$textColor-400 text-white whitespace-nowrap text-$textSize(-3) tracking-tighter leading-0 py-$spacing(-4,1,1.5) px-$spacing(-2,1,1.5) shrink-0 my-auto",
     },
     repeater: {
       outer: "group/repeater [&_.formkit-outer]:mb-0",
