7b6204538990ac7f6eb556defd905de2df173bd6
Andrew Boyd <andrew@formkit.com>
2023-12-05 16:03:22 -0500

    feat: adds styles for summary component

--- src/inputs/form.ts
+++ src/inputs/form.ts
@@ -1,8 +1,33 @@
 export default {
-  // inherits family:text classes
-  form: `group/form`,
-  input: `
-    focus:[&::-webkit-datetime-edit-month-field]:bg-$accentColor-100
-    focus:[&::-webkit-datetime-edit-year-field]:bg-$accentColor-100
+  // https://formkit.com/inputs/form#sections
+  form: ``,
+  actions: ``,
+  summaryInner: `
+    group/summary
+    border
+    border-$colorTemperature-$borderShadeLightMode
+    bg-white
+    $radius(0,rounded-none,rounded-2xl)
+    py-$spacing
+    px-$spacing(2,*)
+    $inputShadow
+    dark:bg-transparent
+    dark:border-$colorTemperature-$borderShadeDarkMode
+  `,
+  summaryHeader: `
+    text-$scale(1,*) 
+    text-$colorTemperature-700
+    font-bold
+    mb-$spacing
+    dark:text-$colorTemperature-300
+  `,
+  messages: ``,
+  message: `
+    group-[]/summary:text-$scale(-1,*)
+  `,
+  messageLink: `
+    group-[]/summary:outline-none
+    group-[]/summary:focus-visible:ring-2
+    group-[]/summary:ring-$accentColor-$baseColorShade
   `,
 };
