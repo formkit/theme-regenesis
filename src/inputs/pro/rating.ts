ab5f8826fbe5da9a2525bb039ce23432efd2e89e
Sasha Milenkovic <sasha@formkit.com>
2024-04-22 08:09:55 -0400

    style: adds styles for rating rewrite

--- src/inputs/pro/rating.ts
+++ src/inputs/pro/rating.ts
@@ -1,32 +1,54 @@
 export default {
   inner: `
-    flex
-    w-[130px]
-    items-center
-    relative
+    text-neutral-400
   `,
   itemsWrapper: `
+    relative
+    inline-flex
+    focus:border-neutral-400
+  `,
+  onItemRow: `
+    h-full
     w-full
   `,
-  onItems: `
-    text-yellow-400
-  `,
-  offItems: `
-    text-$colorTemperature-400
+  offItemRow: `
+    h-full
+    w-full
   `,
   onItemWrapper: `
     [&>*]:w-full
-    [&>*]:h-auto
-    [&>*]:max-w-none
-    [&>*]:max-h-none
+    [&>*]:h-full
+    w-full
+    h-full
+    text-yellow-400
   `,
   offItemWrapper: `
+  text-$colorTemperature-400
+    w-full
+    h-full
     [&>*]:w-full
-    [&>*]:h-auto
-    [&>*]:max-w-none
-    [&>*]:max-h-none
+    [&>*]:h-full
+  `,
+  ratingItem: `
+    relative
+    focus-within:outline
+    w-[1.5em]
+    h-[1.5em]
+  `,
+  itemLabelInner: `
+    h-px
+    w-px
+    overflow-hidden
+    absolute
+    white-space-nowrap
+  `,
+  itemLabel: `
+    absolute
+    h-full
   `,
   ratingIcon: `
-    w-full
+    w-[1.5em]
+    h-[1.5em]
+    flex
   `,
 };
